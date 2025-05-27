9f0b7b4f12cfbc639321f7bb83f9363c5953b6e5
EmilyDiaz1254 <alexandra.edp55@gmail.com>
2025-05-27 10:59:25 -0500

    Ejercicios

--- db/models.py
+++ db/models.py
@@ -1,5 +1,5 @@
 from .database import Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 
 #crear la calse de modelo (identidad)
@@ -9,3 +9,16 @@ class Categoria(Base):
                 primary_key=True
                 )
     nombre = Column(String(50))
+class Categoria(Base):
+    __tablename__ = "productos"
+    nombre = Column(String(40))
+    modelo= Column(String(40))
+    precio = Column(Integer)
+    id = Column(Integer, primary_key=True)
+    Categoria_id = Column(Integer,
+                        ForeignKey("categorias.id")
+                        )
+   
+    
+
+    
